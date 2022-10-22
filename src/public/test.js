9e13d4fadbfe565244132ad9f607e6a3db1c1390
Karl Damus <getaholdofkarl@gmail.com>
2022-10-21 22:53:32 -0400

    Update test.js
    - teamdata
    - schedules
    - player search
    - player data
    - roster

--- src/public/test.js
+++ src/public/test.js
@@ -1,13 +1,12 @@
+// vars and objects
 var xhttp;
+let allPlayers = {};
+// let roster = {};
 
-let filePaths = "constats/filePaths.js";
+// ======
+// ======
 
-// function print hello world
-function test() {
-    console.log('Hello World');
-}
-
-
+// essential filler function
 async function getAllTeamBasicData() {
     let URL = "https://statsapi.web.nhl.com/api/v1/teams";
 
@@ -28,14 +27,68 @@ async function getAllTeamBasicData() {
         Object.entries(allTeamBasicData).sort()
     );
 
-    // for (let team of Object.entries(allTeamBasicData)) {
-    //     console.log(team[1].name + " " + team[1].id + " " + team[1].link);
-    // }
-
-
     return allTeamBasicData;
 }
 
+async function getTeamData(teamID) {
+    let URL = "https://statsapi.web.nhl.com/api/v1/teams/" + teamID;
+
+    const teamData = await getData(URL);
+
+    return teamData;
+}
+
+async function getTeamSchedule(teamID) {
+    let teamData = await getTeamData(teamID);
+
+    // get start and end date
+    let dates = getCurrentWeekScheduleDates();
+    let startDate = dates[0];
+    let endDate = dates[1];
+
+    // get schedule data with teamId, startDate, and endDate
+    let URL = "https://statsapi.web.nhl.com/api/v1/schedule?teamId=" + teamID + "&startDate=" + startDate + "&endDate=" + endDate;
+
+    let teamSchedule = await getData(URL);
+
+    // create array of dates from teamSchedule.dates
+    let datesArray = [];
+    for (let date of Object.entries(teamSchedule.dates)) {
+        datesArray.push(date[1].date);
+    }
+
+    return datesArray;
+}
+
+async function getRosterSchedule() {
+    // get roster from MY_ROSTER.json
+    let roster = await getRoster();
+
+    if (roster == {} || roster == null || roster == undefined) {
+        console.log("roster is empty");
+        return;
+    } else {
+        console.log(roster);
+    } 
+}
+
+function getCurrentWeekScheduleDates() {
+    // start date is the current week starting on monday in the format yyyy-mm-dd
+    let startDate = new Date();
+    startDate.setDate(startDate.getDate() - startDate.getDay() + 1);
+    startDate = startDate.toISOString().split("T")[0];
+    
+    // end date is the sunday after the start date
+    let endDate = new Date(startDate);
+    endDate.setDate(endDate.getDate() + 6);
+    endDate = endDate.toISOString().split("T")[0];
+
+    let dates = [startDate, endDate];
+
+    return dates;
+}
+
+// essential filler function
 async function getAllPlayers() {
     let allTeamBasicData = await getAllTeamBasicData();
 
@@ -55,7 +108,8 @@ async function getAllPlayers() {
                 id: player[1].person.id,
                 name: player[1].person.fullName,
                 link: player[1].person.link,
-                team: team[1].name
+                team: team[1].name,
+                teamID: team[1].id
             }
         }
     }
@@ -63,6 +117,8 @@ async function getAllPlayers() {
     return allPlayers;
 }
 
+// essential filler function
+// addon to getAllPlayers() 
 async function getAllPlayersSorted(sortBy) {
     let allPlayers = await getAllPlayers();
 
@@ -92,8 +148,13 @@ async function getAllPlayersSorted(sortBy) {
     return allPlayers;
 }
 
+// search for a player by name
+// paramater: name - the name of the player to search for. can be a partial name
+// returns an object with the player's name as the key and the player's data as the value
 async function searchForPlayer(name) {
-    let allPlayers = await getAllPlayers();
+    if (Object.keys(allPlayers).length == 0) {
+        allPlayers = await getAllPlayers();
+    }
 
     let playersFound = {};
 
@@ -124,21 +185,102 @@ async function searchForPlayer(name) {
     return playersFound;
 }
 
+// add the given player to the MY_ROSTER.txt file
+async function addPlayerToRosterFile(player) {
+    xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = async function() {
+        if (this.readyState == 4 && this.status == 200) {
+            console.log("Added " + player.name + " to MY_ROSTER.json");
+        }
+    };
+
+    console.log("Attempting to add " + player.name + " to MY_ROSTER.json");
+
+    // send GET request to server to add player to MY_ROSTER.json
+    xhttp.open("GET", "http://localhost:3000/addPlayerToRoster?name=" + player.name + "&id=" + player.id + "&team=" + player.team, true);
+    xhttp.send();
+}
+
+const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms))
+
+// get the current roster from the MY_ROSTER.json file
+async function getRosterSchedule() {
+    xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = async function() {
+        if (this.readyState == 4 && this.status == 200) {
+            let roster = JSON.parse(xhttp.responseText);
+
+            console.log("Roster created, returning");
+            return roster;
+        } else {
+            await sleep(1000);
+        }
+    };
+
+    // send GET request to server to get MY_ROSTER.json
+    xhttp.open("GET", "http://localhost:3000/getRoster", true);
+    xhttp.send();
+}
+
+// based on the names of players in rosterNames.txt, add those players to MY_ROSTER.json
+async function createRoster() {
+    xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = async function() {
+        if (this.readyState == 4 && this.status == 200) {
+            let names = JSON.parse(this.responseText);
+
+            for (let name of names) {
+                player = await searchForPlayer(name);
+                console.log(player);
+                await addPlayerToRosterFile(player[Object.keys(player)[0]]);
+            }
+        }
+    };
+
+    // send GET request to server to create MY_ROSTER.json
+    xhttp.open("GET", "http://localhost:3000/createRoster", true);
+    xhttp.send();
+}
+
+function getLocalRoster() {
+    // send request to server to get MY_ROSTER.json
+    xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            console.log(this.responseText);
+        }
+    }
+
+    xhttp.open("GET", "http://localhost:3000/getRoster", true);
+    xhttp.send();
+
+}
+
+// print the given player's data
 function printPlayers(players) {
     for (let player of Object.entries(players)) {
         console.log(player[1].name + " " + player[1].id + " " + player[1].team);
     }
 }
 
+// print the given player's data
 function printPlayer(player) {
     console.log(player.name + " " + player.id + " " + player.team);
 }
 
+// testing
 async function main() {
-    let players = await getAllPlayersSorted("id");
-    printPlayers(players);
+    console.log("Before getting roster");
+    roster = await getRoster();
+
+
+
+    console.log("After getting roster");
+    console.log(roster);
+    // console.log(rosterSchedule);
 }
 
+// fetch data from the given URL
 async function getData(URL) {
     let response = await fetch(URL);
     let data = await response.json();
