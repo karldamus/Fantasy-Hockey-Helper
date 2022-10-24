b801d2b1c58d4a1b31d0b2d06129ffd25c74fed2
Karl Damus <getaholdofkarl@gmail.com>
2022-10-23 21:22:04 -0400

    Update first.js
    Move getDayOfWeekAsWord method

--- src/public/first.js
+++ src/public/first.js
@@ -7,3 +7,27 @@ async function getData(URL) {
     let data = await response.json();
     return data;
 }
+
+
+async function getDayOfWeekAsWord(date) {
+    let dayOfWeek = new Date(date).getDay();
+
+    switch (dayOfWeek) {
+        case 0:
+            return "Monday";
+        case 1:
+            return "Tuesday";
+        case 2:
+            return "Wednesday";
+        case 3:
+            return "Thursday";
+        case 4:
+            return "Friday";
+        case 5:
+            return "Saturday";
+        case 6:
+            return "Sunday";
+        default:
+            return "Error";
+    }
+}
