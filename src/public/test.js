b7915b31a1eb64645ffbad2680098ef05c556547
Karl Damus <getaholdofkarl@gmail.com>
2022-10-23 21:23:12 -0400

    Update test.js

--- src/public/test.js
+++ src/public/test.js
@@ -1,26 +1,25 @@
 // vars and objects
 var xhttp;
-let allPlayers = {};
-// let roster = {};
+// let allPlayers = {};
 
-// ======
-// ======
-
-
+async function testYahooApi() {
+    // get yahoo secret
+}
 
 
+async function getNewAccessToken() {
+    xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = async function() {
+        if (this.readyState == 4 && this.status == 200) {
+            console.log("Got new access token");
+            console.log(this.responseText);
+        }
+    };
 
+    // send POST request to get new access token
+    xhttp.open("POST", "https://api.login.yahoo.com/oauth2/get_token", true);
+    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhttp.send("client_id=" + secrets.client_id + "&client_secret=" + secrets.client_secret + "&redirect_uri=" + secrets.redirect_uri + "&refresh_token=" + secrets.refresh_token + "&grant_type=refresh_token");
 
-
-// // testing
-// async function main() {
-//     console.log("Before getting roster");
-//     roster = await getRoster();
-
-
-
-//     console.log("After getting roster");
-//     console.log(roster);
-//     // console.log(rosterSchedule);
-// }
-
+    console.log(xhttp.responseText);
+}
